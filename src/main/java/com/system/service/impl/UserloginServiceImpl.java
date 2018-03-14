ae102db128b1f69e7f8c151ba1a3bad7cf8a0970
Your Name <you@example.com>
2018-03-14 16:50:48 +0800

    我想要提交上去啊

---
+++ src/main/java/com/system/service/impl/UserloginServiceImpl.java
@@ -0,0 +1,58 @@
+package com.system.service.impl;
+
+import com.system.mapper.UserloginMapper;
+import com.system.po.Userlogin;
+import com.system.po.UserloginExample;
+import com.system.service.UserloginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by Jacey on 2017/6/29.
+ */
+@Service
+public class UserloginServiceImpl implements UserloginService {
+
+    @Autowired
+    private UserloginMapper userloginMapper;
+
+
+  //根据名字查找用户
+    public Userlogin findByName(String name) throws Exception {
+        UserloginExample userloginExample = new UserloginExample();
+
+        UserloginExample.Criteria criteria = userloginExample.createCriteria();
+        criteria.andUsernameEqualTo(name);
+
+        List<Userlogin> list = userloginMapper.selectByExample(userloginExample);
+
+        return list.get(0);
+    }
+
+//保存用户登录信息
+    public void save(Userlogin userlogin) throws Exception {
+        userloginMapper.insert(userlogin);
+    }
+
+    public void removeByName(String name) throws Exception {
+        UserloginExample userloginExample = new UserloginExample();
+
+        UserloginExample.Criteria criteria = userloginExample.createCriteria();
+        criteria.andUsernameEqualTo(name);
+
+        userloginMapper.deleteByExample(userloginExample);
+    }
+
+    @Override
+    public void updateByName(String name, Userlogin userlogin) {
+        UserloginExample userloginExample = new UserloginExample();
+
+        UserloginExample.Criteria criteria = userloginExample.createCriteria();
+        criteria.andUsernameEqualTo(name);
+
+        userloginMapper.updateByExample(userlogin, userloginExample);
+    }
+
+}
